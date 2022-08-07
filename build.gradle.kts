e766b341a30483116a0f07c4313bafd1f20f713a
Iseason2000 <65019366+Iseason2000@users.noreply.github.com>
2022-08-07 21:35:43 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,149 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.guardsquare:proguard-gradle:7.2.2")
+    }
+}
+// 插件名称，请在gradle.properties 修改
+val pluginName: String by project
+//包名，请在gradle.properties 修改
+val group: String by project
+val groupS = group
+// 作者，请在gradle.properties 修改
+val author: String by project
+// jar包输出路径，请在gradle.properties 修改
+val jarOutputFile: String by project
+//插件版本，请在gradle.properties 修改
+val version: String by project
+// shadowJar 版本 ，请在gradle.properties 修改
+val shadowJar: ShadowJar by tasks
+// exposed 数据库框架版本，请在gradle.properties 修改
+val exposedVersion: String by project
+repositories {
+//    阿里的服务器速度快一点
+    maven {
+        name = "aliyun"
+        url = uri("https://maven.aliyun.com/repository/public/")
+    }
+    google()
+    mavenCentral()
+    maven {
+        name = "spigot"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/public/")
+    }
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+
+    mavenLocal()
+}
+
+dependencies {
+    //基础库
+    compileOnly(kotlin("stdlib-jdk8"))
+//    反射库
+//    compileOnly(kotlin("reflect"))
+
+//    协程库
+//    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+
+    // 数据库
+    compileOnly("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    compileOnly("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    compileOnly("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    implementation("org.bstats:bstats-bukkit:3.0.0")
+    compileOnly("org.spigotmc:spigot-api:1.13-R0.1-SNAPSHOT")
+
+}
+
+
+tasks {
+    shadowJar {
+        relocate("top.iseason.bukkit.bukkittemplate", "$groupS.libs.core")
+    }
+    build {
+        dependsOn("buildPlugin")
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(
+                "main" to "$groupS.libs.core.BukkitTemplate",
+                "name" to pluginName,
+                "version" to project.version,
+                "author" to author,
+                "kotlinVersion" to getProperties("kotlinVersion"),
+                "exposedVersion" to exposedVersion
+            )
+        }
+    }
+}
+task<com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation>("relocateShadowJar") {
+    target = tasks.shadowJar.get()
+    prefix = "$groupS.libs"
+    shadowJar.minimize()
+}
+tasks.shadowJar.get().dependsOn(tasks.getByName("relocateShadowJar"))
+
+tasks.register<proguard.gradle.ProGuardTask>("buildPlugin") {
+    group = "minecraft"
+    verbose()
+    injars(tasks.named("shadowJar"))
+    //是否混淆，注销掉启用混淆
+    val obfuscated = getProperties("obfuscated") == "true"
+    val shrink = getProperties("shrink") == "true"
+    if (!obfuscated) {
+        dontobfuscate()
+    }
+    if (!shrink) {
+        dontshrink()
+    }
+    optimizationpasses(5)
+    dontwarn()
+    val javaHome = System.getProperty("java.home")
+    if (JavaVersion.current() < JavaVersion.toVersion(9)) {
+        libraryjars("$javaHome/lib/rt.jar")
+    } else {
+        libraryjars(
+            mapOf(
+                "jarfilter" to "!**.jar",
+                "filter" to "!module-info.class"
+            ),
+            "$javaHome/jmods/java.base.jmod"
+        )
+    }
+    val allowObf = mapOf("allowobfuscation" to true)
+    libraryjars(configurations.compileClasspath.get().files)
+    keep("class $groupS.libs.core.BukkitTemplate {}")
+    keep(allowObf, "class * implements $groupS.libs.core.KotlinPlugin {*;}")
+    keepclassmembers("class * extends $groupS.libs.core.config.SimpleYAMLConfig {*;}")
+    keepclassmembers(allowObf, "class * implements org.bukkit.event.Listener {*;}")
+    keep(allowObf, "class $groupS.libs.core.utils.MessageKt {*;}")
+    keepattributes("Exceptions,InnerClasses,Signature,Deprecated,SourceFile,LineNumberTable,*Annotation*,EnclosingMethod")
+    keepkotlinmetadata()
+    repackageclasses()
+    if (obfuscated)
+        outjars(File(jarOutputFile, "${project.name}-${project.version}-obfuscated.jar"))
+    else
+        outjars(File(jarOutputFile, "${project.name}-${project.version}.jar"))
+}
+
+fun getProperties(properties: String) = rootProject.properties[properties].toString()
---
+++ src/main/kotlin/top/iseason/bukkit/bukkittemplate/config/ConfigWatcher.kt
@@ -0,0 +1,69 @@
+package top.iseason.bukkit.bukkittemplate.config
+
+import org.bukkit.scheduler.BukkitRunnable
+import top.iseason.bukkit.bukkittemplate.BukkitTemplate
+import java.io.File
+import java.lang.Thread.sleep
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import java.nio.file.StandardWatchEventKinds
+import java.nio.file.WatchService
+
+class ConfigWatcher private constructor(private val folder: File) : BukkitRunnable() {
+    private var isEnable = true
+    private val service: WatchService = FileSystems.getDefault().newWatchService()
+        .apply { folder.toPath().register(this, StandardWatchEventKinds.ENTRY_MODIFY) }
+
+    override fun run() {
+        while (isEnable) {
+            val key = try {
+                service.take()
+            } catch (e: Exception) {
+                break
+            }
+            //监听文件修改
+            event@ for (pollEvent in key.pollEvents()) {
+                if (pollEvent.kind() != StandardWatchEventKinds.ENTRY_MODIFY) continue
+                val path = pollEvent.context() as Path
+                val absolutePath = "${folder}${File.separatorChar}$path"
+                val simpleYAMLConfig = SimpleYAMLConfig.configs[absolutePath] ?: continue
+                if (simpleYAMLConfig.isAutoUpdate) {
+                    sleep(200)
+                    simpleYAMLConfig.load()
+                    service.poll()?.reset()
+                    key.reset()
+                    break@event
+                }
+            }
+        }
+    }
+
+    override fun cancel() {
+        super.cancel()
+        isEnable = false
+        service.close()
+    }
+
+    companion object {
+        private val folders = mutableMapOf<String, ConfigWatcher>()
+
+        fun fromFile(file: File): ConfigWatcher {
+            val parentFile = file.absoluteFile.parentFile
+            val folder = parentFile.toString()
+            val existWatcher = folders[folder]
+            if (existWatcher != null) return existWatcher
+            val configWatcher = ConfigWatcher(parentFile)
+            folders[folder] = configWatcher
+            configWatcher.runTaskAsynchronously(BukkitTemplate.getPlugin())
+            return configWatcher
+        }
+
+        fun onDisable() {
+            for (v in folders.values) {
+                v.cancel()
+            }
+            folders.clear()
+        }
+
+    }
+}
